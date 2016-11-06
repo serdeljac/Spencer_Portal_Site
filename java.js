f6f932f263230c3fe7ad37341a9b110d0cd36ea2
Stjepan J. Erdeljac <serdeljac88@gmail.com>
2016-11-06 12:16:35 -0800

    New format

---
+++ java.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	$('#missingdog').hover(
+		function() {$(this).attr("src", "img/img_link_missing-dog-hov.png")},
+		function() {$(this).attr("src", "img/img_link_missing-dog.png")}
+	);
+	
+	$('#take').hover(
+		function() {$(this).attr("src", "img/img_link_take-hov.png")},
+		function() {$(this).attr("src", "img/img_link_take.png")}
+	);
+	
+	$('#art').hover(
+		function() {$(this).attr("src", "img/img_link_art-hov.png")},
+		function() {$(this).attr("src", "img/img_link_art.png")}
+	);
+	
+	$('#mage').hover(
+		function() {$(this).attr("src", "img/img_link_mage-hov.png")},
+		function() {$(this).attr("src", "img/img_link_mage.png")}
+	);
+	
+	$('#ash').hover(
+		function() {$(this).attr("src", "img/img_link_ash-hov.png")},
+		function() {$(this).attr("src", "img/img_link_ash.png")}
+	);
+	
+	$('#scream').hover(
+		function() {$(this).attr("src", "img/img_link_scream-hov.png")},
+		function() {$(this).attr("src", "img/img_link_scream.png")}
+	);
+	
+	$('#dtf').hover(
+		function() {$(this).attr("src", "img/img_dtf-hov.jpg")},
+		function() {$(this).attr("src", "img/img_dtf.jpg")}
+	);
+	
+	$('#beast').hover(
+		function() {$(this).attr("src", "img/img_beast-hov.jpg")},
+		function() {$(this).attr("src", "img/img_beast.jpg")}
+	);
+
+});
