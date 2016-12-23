5bf6e018d20bf10245f6be55a81bcb5c46a6e4b1
Stjepan J. Erdeljac <serdeljac88@gmail.com>
2016-12-22 23:07:15 -0800

    Image Optimization and more Responsive

--- java.js
+++ java.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
 	$('#missingdog').hover(
-		function() {$(this).attr("src", "img/img_link_missing-dog-hov.png")},
-		function() {$(this).attr("src", "img/img_link_missing-dog.png")}
+		function() {$(this).attr("src", "img/img_link_missing-dog-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_missing-dog.jpg")}
 	);
 	
 	$('#take').hover(
-		function() {$(this).attr("src", "img/img_link_take-hov.png")},
-		function() {$(this).attr("src", "img/img_link_take.png")}
+		function() {$(this).attr("src", "img/img_link_take-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_take.jpg")}
 	);
 	
 	$('#art').hover(
-		function() {$(this).attr("src", "img/img_link_art-hov.png")},
-		function() {$(this).attr("src", "img/img_link_art.png")}
+		function() {$(this).attr("src", "img/img_link_art-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_art.jpg")}
 	);
 	
 	$('#mage').hover(
-		function() {$(this).attr("src", "img/img_link_mage-hov.png")},
-		function() {$(this).attr("src", "img/img_link_mage.png")}
+		function() {$(this).attr("src", "img/img_link_mage-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_mage.jpg")}
 	);
 	
 	$('#ash').hover(
-		function() {$(this).attr("src", "img/img_link_ash-hov.png")},
-		function() {$(this).attr("src", "img/img_link_ash.png")}
+		function() {$(this).attr("src", "img/img_link_ash-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_ash.jpg")}
 	);
 	
 	$('#scream').hover(
-		function() {$(this).attr("src", "img/img_link_scream-hov.png")},
-		function() {$(this).attr("src", "img/img_link_scream.png")}
+		function() {$(this).attr("src", "img/img_link_scream-hov.jpg")},
+		function() {$(this).attr("src", "img/img_link_scream.jpg")}
 	);
 	
 	$('#dtf').hover(
